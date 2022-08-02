f0536a9c282803d8c5532889249fac1b9c39302c
AshRahman <ashfaqurrahmanrahat@outlook.com>
2022-08-02 20:07:43 +0600

    almost complete

--- FileManagement.py
+++ FileManagement.py
@@ -1,26 +1,43 @@
 import os
+import shutil
 import sys
 import time
 import logging
 
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
 
 source_dir ="/Users/intern3.bpl/Downloads"
-dest_dir_images = "/Users/intern3.bpl/DownloadedImages"
+dest_dir_images = "/Users/intern3.bpl/DownloadedImages" ## Need to implement Environment or .env
+
+source_dir_home ="/Users/Rocka/Downloads"
+dest_dir_images_home = "/Users/Rocka/DownloadedImages"
+
+def move(dest, entry,name):
+    file_exists = os.path.exists(dest+"/"+name)
+    if file_exists:
+        unique_name= makeUnique(name)
+        os.rename(entry,unique_name)
+    shutil.move(entry,dest)
 
 class MoverHandler(FileSystemEventHandler):
     def on_modified(self,event):
         with os.scandir(source_dir) as entries:
             for entry in entries:
-                print(entry.name)
+                name  = entry.name
+                dest=source_dir
+                
+                if name.endswith('.jpg') or name.endswith('.jpeg') or name.endswith('.jpg'):
+                    dest = dest_dir_images
+                    move(dest,entry,name)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+    
+    path = source_dir
+    event_handler = MoverHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
