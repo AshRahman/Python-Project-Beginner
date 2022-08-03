f229713ceed0459abda11a808c110ebba57e4563
AshRahman <ashfaqurrahmanrahat@outlook.com>
2022-08-03 11:20:17 +0600

    added some comments

--- FileManagement.py
+++ FileManagement.py
@@ -25,7 +25,7 @@ def make_unique(path):
         
     return path 
 
-
+### Mover function 
 def move(dest, entry,name):
     file_exists = os.path.exists(dest+"/"+name)
     if file_exists:
@@ -39,18 +39,18 @@ class MoverHandler(FileSystemEventHandler):
             for entry in entries:
                 name  = entry.name
                 dest=source_dir
-                
-                if name.endswith('.jpg') or name.endswith('.jpeg') or name.endswith('.jpg') or name.endswith('.bmp'):
+                ### This checks the extensions to redirect
+                if name.endswith('.jpg') or name.endswith('.jpeg') or name.endswith('.jpg') or name.endswith('.bmp'): 
                     dest = dest_dir_images
                     move(dest,entry,name)
-
+#### Base Watchdog module for continuious checking for directory items change
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     
-    path = source_dir
-    event_handler = MoverHandler()
+    path = source_dir  ### path should be changed to source directory
+    event_handler = MoverHandler() ### this is where the change will happen according to the class name
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
